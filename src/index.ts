361a42b7e3c0d836c8ee121f632f623e7e0ca743
Philip Rutberg <philip.rutberg@postnord.com>
2024-04-01 14:09:05 +0200

    entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
-import Redis from 'ioredis';
+import connect from './connection';
+import benchmarks from './benchmarks';
 
-const redis = new Redis('redis://redis:6379');
+import * as dotenv from 'dotenv';
+import { BenchmarkTableResult } from './types';
+import { calculateAverageOpsPerSec, sortResults } from './helpers';
+dotenv.config();
 
-redis.call('JSON.SET', 'test', '$', JSON.stringify({ foo: 'bar' }));
-redis.call('JSON.GET', 'test').then((res) => {
-  console.log(res);
-});
+async function main() {
+  await connect();
+
+  const results: BenchmarkTableResult[] = [];
+  for (const benchmark of benchmarks) {
+    const result = await benchmark();
+    results.push(...result);
+  }
+
+  const sorted = sortResults(results);
+  console.info('Benchmark results:');
+  console.table(sorted);
+
+  const avgOpsPerSec = calculateAverageOpsPerSec(sorted);
+  console.info('Average ops/sec:');
+  console.table(avgOpsPerSec);
+
+  const maxOpsPerSec = avgOpsPerSec.reduce((max, result) => {
+    return result.avgOpsPerSec > max.avgOpsPerSec ? result : max;
+  });
+  console.info('Most performant:');
+  console.table(maxOpsPerSec);
+}
+
+main().catch(console.error);
